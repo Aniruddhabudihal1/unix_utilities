50861ba3274efbfb85c501d72d94c4f0144b6eaa
Aniruddha Budihal <aniruddhabudihal@gmail.com>
2025-07-12 10:15:07 +0530

    basic working wgrep.

--- wgrep.c
+++ wgrep.c
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-void find(const char *buffer, long buf_size, char *word);
+void find(char *, long, char *);
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
     long file_size = ftell(file_des);
     rewind(file_des);
 
-    FILE *buffer = malloc(file_size);
+    FILE *buffer = (FILE *)malloc(file_size);
 
     if (buffer == NULL) {
       fprintf(stderr, "something went wrong when allocating space for the "
@@ -46,38 +46,42 @@ int main(int argc, char *argv[]) {
       continue;
     }
 
-    char buffer2[file_size];
     read_status = fread(buffer, sizeof(char), file_size, file_des);
-    strcpy(buffer2, (char *)buffer);
+    char *buffer2 = (char *)malloc(file_size + 1);
 
+    strcpy(buffer2, (char *)buffer);
     find(buffer2, file_size, word);
   }
   return 0;
 }
 
-void find(const char *buffer, long buf_size, char *word) {
-  char *line_buf = malloc(buf_size);
-  int len = sizeof word;
-  int return_flag = 0;
-
-  char *bait[100];
-  for (int i = 0; i < buf_size; i++) {
-    if (buffer[i] == '\n') {
-      if (return_flag) {
-        printf("%s\n", line_buf);
+void find(char main_buffer[], long size, char *word_to_be_found) {
+  long target_length = strlen(word_to_be_found);
+  int word_found = 0;
+  char *temp = (char *)malloc(size);
+  char *line_buff = (char *)malloc(size);
+  for (int i = 0; i < size; i++) {
+    if (main_buffer[i] == '\n') {
+      if (word_found) {
+        printf("%s\n", line_buff);
+        word_found = 0;
       }
-      return_flag = 0;
-      strcpy(line_buf, "");
+      strcpy(temp, "");
+      strcpy(line_buff, "");
     }
-
-    if (buffer[i] != '\0') {
-      printf("%c\n", buffer[i]);
-      strcat(*bait, &buffer[i]);
-      // sprintf(*bait, "%c", buffer[i]);
+    printf("%s\n", line_buff);
+    long temp_len = strlen(temp);
+    if (temp_len == target_length) {
+      if (strcmp(word_to_be_found, temp) == 0) {
+        word_found = 1;
+      }
     }
-    if (strcmp(*bait, word)) {
-      return_flag = 1;
+    if (main_buffer[i] == ' ') {
+      strcat(line_buff, temp);
+      strcat(line_buff, " ");
+      strcat(temp, "");
+      continue;
     }
+    strcat(temp, &main_buffer[i]);
   }
-  free(line_buf);
 }
